d74a450fa8b021fddb46487d7baaabfa5489e0c2
vasudev <dev.vasu74@gmail.com>
2024-06-21 16:47:50 +0530

    dhb

---
+++ day2-customNw/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+    description = "passing Ami value"
+    type = string
+    default = ""
+
+  
+}
+variable "instance_type" {
+    description = "calling instance type"
+    type = string
+    default = ""
+
+  
+}
+variable "key_name" {
+    description = "key_name "
+    type = string
+    default = ""
+  
+}
